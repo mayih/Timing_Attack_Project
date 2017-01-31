3fa4ebc76eba7976e98c56a9a843d56b2f5e9464
mayih <u.mayih@gmail.com>
2017-01-31 19:40:51 +0200

    Step 2 Done

---
+++ Random_Timing_Attack/Random_Timing_Attack.ino
@@ -0,0 +1,62 @@
+#include "KeypadEmulation.h"
+
+KeypadEmulation keypadS;
+unsigned short maximums[4] = {0};
+void setup() {
+  // initialize the digital pin as an output.
+
+  keypadS = KeypadEmulation();
+  pinMode(PB2, INPUT_PULLUP);
+  delay(1000);
+  Serial.begin(9600);
+}
+void maximum(unsigned short pos)
+{
+  int t = 0;
+  double timeSum = 0, averages[12] = {0};
+  for (maximums[pos] = 0; maximums[pos] < 12; maximums[pos]++)
+  {
+    timeSum = 0;
+    for (int j = 0; j < 80; j++)
+    {
+      
+      t = keypadS.inputSimulation((unsigned short)maximums[0], (unsigned short)maximums[1],
+                                  (unsigned short)maximums[2], (unsigned short)maximums[3]) ; 
+                                  //Serial.println(t);   
+      timeSum = timeSum + (t);
+      t = 0;
+    }
+    averages[maximums[pos]] = (timeSum / 80.00);
+    
+    Serial.println(averages[maximums[pos]]);
+    delay(1000);
+  }
+  maximums[pos] = 0;
+  for (int i = 0; i < 11; i++)
+  {
+    if (averages[maximums[pos]] < averages[i + 1]) {
+      maximums[pos] = i + 1;
+    }
+  }
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+//delay(600000);
+  for (int i = 0; i < 4; i++)
+  {
+    maximum(i);
+
+    Serial.println(maximums[i]);
+    delay(1);
+  }
+keypadS.inputSimulation((unsigned short)maximums[0], (unsigned short)maximums[1],
+                                  (unsigned short)maximums[2], (unsigned short)maximums[3]) ;
+  for(int i = 0; i < 4; i++)
+  {
+    maximums[i] = 0;
+  }
+  delay(60000);
+
+  Serial.println();
+}
---
+++ Target_2/Target_2.ino
@@ -0,0 +1,98 @@
+#include "Keypad.h"
+#include <LiquidCrystal.h>
+#define RS 8
+#define E 7
+#define D4 6
+#define D5 5
+#define D6 4
+#define D7 3
+#define LCDCOLS 20
+#define LCDROWS 4
+
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
+
+const byte ledpin = 12;
+
+bool secretPass(char userInput[4]);
+void setUserInput();
+char userInput[4];
+int i = 0, j = 0;
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+byte rowPins[ROWS] = {14, 15, 16, 17}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {18, 19, 2}; //connect to the column pinouts of the keypad
+Keypad mykeypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup() {
+  pinMode(ledpin, OUTPUT);
+  Serial.begin(9600);
+  lcd.begin(LCDCOLS, LCDROWS);
+  lcd.setCursor(0, 0);
+  lcd.print("Put your password:");
+}
+
+void loop()
+{
+  setUserInput();
+}
+
+void setUserInput()
+{
+  char key = mykeypad.getKey();
+
+  if (key != NO_KEY)
+  {
+    if (i == 0) lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Put your password:");
+    lcd.setCursor(i % 4, 1);
+    lcd.print(key);
+   // Serial.println(userInput[0]);
+    userInput[i % 4] = key;
+    if (sizeof(userInput) > 4) exit(0); //check input
+
+    i++;
+    if (i > 3)
+    {
+      i = 0;
+      j++;
+
+      //delayMicroseconds(1);
+      
+      //delay(500);
+      //Serial.println(userInput);
+      lcd.setCursor(0, 2);
+      if (secretPass(userInput))
+      {
+        lcd.print("Access granted");
+      }
+      else
+      {
+        lcd.print("Acess denied");
+      }
+      
+     }
+  }
+}
+bool secretPass(char userInput[4])
+{
+  
+  char passw[4] = "9315";
+  bool isEqual;
+  int r = random(0,5);
+  //Serial.println(r);
+  delay(1);
+  digitalWrite(ledpin, HIGH);
+  isEqual = !strncmp(passw, userInput, 4);
+  delayMicroseconds(r);
+  digitalWrite(ledpin, LOW);
+  
+  return isEqual;
+}
